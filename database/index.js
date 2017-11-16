1c251131ea022a5f3bb668e446e659202e8c0bed
Kaylin <kaylin.collins68@gmail.com>
2017-11-16 15:19:54 -0800

    set up to sequelize

--- database/index.js
+++ database/index.js
@@ -1,48 +1,61 @@
 const mysql = require('mysql');
-//const movieAPI = require('../lib/movieAPI.js');
+const Sequelize = require('sequelize');
 
-const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  database : 'movielist'
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 
-module.exports.saveAllData = (req, response, next) => {
-  for(var i = 0; i < req.data.length; i++) {
-    var query = `INSERT INTO movies (title, description) VALUES ("${req.data[i].title}", "${req.data[i].overview}")`
-    connection.query(query, (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('saved!');
-      }
-    })
-  }
-  response.send(JSON.stringify(req.data))
-}
+// const connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   database : 'movielist'
+// });
 
-module.exports.saveMovie = (req, res, next) => {
+
+// module.exports.saveAllData = (req, response, next) => {
+//   for(var i = 0; i < req.data.length; i++) {
+//     var query = `INSERT INTO movies (title, description) VALUES ("${req.data[i].title}", "${req.data[i].overview}")`
+//     connection.query(query, (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log('saved!');
+//       }
+//     })
+//   }
+//   response.send(JSON.stringify(req.data))
+// }
+
+// module.exports.saveMovie = (req, res, next) => {
   
-  var query = `INSERT INTO movies (title) VALUES ("${req.body.title}")`
-  connection.query(query, (err, data) => {
-    if (err) {
-      res.status(500).send({ error: err });
-    } else {
-      next();
-    }
-  }) 
-}
+//   var query = `INSERT INTO movies (title) VALUES ("${req.body.title}")`
+//   connection.query(query, (err, data) => {
+//     if (err) {
+//       res.status(500).send({ error: err });
+//     } else {
+//       next();
+//     }
+//   }) 
+// }
 
-module.exports.sendAllMovies = (req, res, next) => {
-  var query = `SELECT * FROM movies`;
-  connection.query(query, (err, data) => {
-    if (err) {
-      res.status(500).send({ error: err });
-    } else {
-      console.log(data);
-      res.status(201).send(JSON.stringify(data));
-    }
-  })
-}
+// module.exports.sendAllMovies = (req, res, next) => {
+//   var query = `SELECT * FROM movies`;
+//   connection.query(query, (err, data) => {
+//     if (err) {
+//       res.status(500).send({ error: err });
+//     } else {
+//       console.log(data);
+//       res.status(201).send(JSON.stringify(data));
+//     }
+//   })
+// }
 
